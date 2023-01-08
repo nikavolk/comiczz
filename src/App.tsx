4140603600c1c8a9f3123190e1827bd49e999a82
Nika V <76059172+nikavolk@users.noreply.github.com>
2023-01-08 16:14:14 +0100

    Basic header code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { Button } from "./components/Button";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      <header>
+        <Header />
+      </header>
       <h1>Hello world</h1>
       <Button type="submit">Hello</Button>
     </div>
